61895e19c1cf92a3e7a258e5452351b329e0a1d1
Frank1604 <frank@w-mann.de>
2015-01-07 08:51:46 +0100

    Translation

--- l10n/de.php
+++ l10n/de.php
@@ -1,3 +1,17 @@
-<?php $TRANSLATIONS = array(
-	'Close' => 'Schließen',
+<?php
+$TRANSLATIONS = array(
+		'Close'       => 'Schließen',
+		'Description' => 'Beschreibung',
+		'Date'        => 'Datum',
+		'Start'       => 'Startzeit',
+		'Duration'    => 'Dauer',
+		'Distance'    => 'Distanz',
+		'Pace'        => 'Pace',
+		'Heartrate'   => 'Herzfreq.',
+		'Altitude'    => 'Höhenmeter',
+		'Height'      => 'Höhe',
+		'Lap times'   => 'Rundenzeiten',
+		'Lap time'    => 'Rundenzeit',
+		'Time'        => 'Zeit',
+		'Speed'       => 'Geschwindigkeit',
 );
