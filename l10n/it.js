fbc3a57ec12aff6c49e0801d46a04a832e2f58e7
Lorenzo Milesi <maxxer@yetopen.it>
2015-03-18 12:57:51 +0100

    Italian translation

---
+++ l10n/it.js
@@ -0,0 +1,19 @@
+OC.L10N.register(
+    "files_gpxviewer_extended",
+    {
+		'Close' : 'Chiudi',
+		'Description' : 'Descrizione',
+		'Date'  : 'Data',
+		'Start' : 'Inizio',
+		'Duration' : 'Durata',
+		'Distance' : 'Distanza',
+		'Pace'  : 'Ritmo',
+		'Heartrate' : 'Battito cardiaco',
+		'Altitude' : 'Altitudine',
+		'Height' : 'Altitudine',
+		'Lap times' : 'Tempi giro',
+		'Lap time' : 'Tempo giro',
+		'Time'  : 'Tempo',
+		'Speed' : 'Velocit&agrave;'
+	},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/it.php
@@ -0,0 +1,17 @@
+<?php
+$TRANSLATIONS = array(
+		'Close'       => 'Chiudi',
+		'Description' => 'Descrizione',
+		'Date'        => 'Data',
+		'Start'       => 'Inizio',
+		'Duration'    => 'Durata',
+		'Distance'    => 'Distanza',
+		'Pace'        => 'Ritmo',
+		'Heartrate'   => 'Battito cardiaco',
+		'Altitude'    => 'Altitudine',
+		'Height'      => 'Altitudine',
+		'Lap times'   => 'Tempi giro',
+		'Lap time'    => 'Tempo giro',
+		'Time'        => 'Tempo',
+		'Speed'       => 'Velocit&agrave;',
+);
