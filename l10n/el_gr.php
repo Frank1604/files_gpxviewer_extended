0ae89c33ebe4daa0f2a95fff129001fcbbbb7c8a
C-Makridis <chris@cmakridis.gr>
2015-03-31 22:01:51 +0300

    Create el_gr.php
    Greek Translation

---
+++ l10n/el_gr.php
@@ -0,0 +1,17 @@
+<?php
+$TRANSLATIONS = array(
+		'Close'       => 'Κλείσιμο',
+		'Description' => 'Περιγραφή',
+		'Date'        => 'Ημερομηνία',
+		'Start'       => 'Έναρξη',
+		'Duration'    => 'Διάρκεια',
+		'Distance'    => 'Απόσταση',
+		'Pace'        => 'Ρυθμός',
+		'Heartrate'   => 'Σφυγμοί',
+		'Altitude'    => 'Ανάβαση',
+		'Height'      => 'Υψόμετρο',
+		'Lap times'   => 'Χρόνοι γύρων',
+		'Lap time'    => 'Χρόνος γύρου',
+		'Time'        => 'Χρόνος',
+		'Speed'       => 'Ταχύτητα',
+);
