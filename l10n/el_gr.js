a889d6369cae3589c6e221aa03a478fb43d814da
C-Makridis <chris@cmakridis.gr>
2015-03-31 22:02:39 +0300

    Create el_gr.js
    Greek Translation

---
+++ l10n/el_gr.js
@@ -0,0 +1,19 @@
+OC.L10N.register(
+    "files_gpxviewer_extended",
+    {
+		'Close' : 'Κλείσιμο',
+		'Description' : 'Περιγραφή',
+		'Date'  : 'Ημερομηνία',
+		'Start' : 'Έναρξη',
+		'Duration' : 'Διάρκεια',
+		'Distance' : 'Απόσταση',
+		'Pace'  : 'Ρυθμός',
+		'Heartrate' : 'Σφυγμοί',
+		'Altitude' : 'Ανάβαση',
+		'Height' : 'Υψόμετρο',
+		'Lap times' : 'Χρόνοι γύρων',
+		'Lap time' : 'Χρόνος γύρου',
+		'Time'  : 'Χρόνος',
+		'Speed' : 'Ταχύτητα'
+	},
+"nplurals=2; plural=(n != 1);");
