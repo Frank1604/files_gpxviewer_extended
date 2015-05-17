db204849a4f7182fa991a58363d3335891c74b6a
Juan Roldan Ruiz <juan@shoden.es>
2015-05-17 13:30:47 +0200

    Spanish translation

---
+++ l10n/es.js
@@ -0,0 +1,19 @@
+OC.L10N.register(
+    "files_gpxviewer_extended",
+    {
+		'Close' : 'Cerrar',
+		'Description' : 'Descripci&oacute;n',
+		'Date'  : 'Fecha',
+		'Start' : 'Comienzo',
+		'Duration' : 'Duraci&oacute;n',
+		'Distance' : 'Distancia',
+		'Pace'  : 'Ritmo',
+		'Heartrate' : 'Ritmo card&iacute;aco',
+		'Total Ascend' : 'Ascenso total',
+		'Height' : 'Altitud',
+		'Lap times' : 'Tiempo vueltas',
+		'Lap time' : 'Tiempo vuelta',
+		'Time'  : 'Tiempo',
+		'Speed' : 'Velocidad'
+	},
+"nplurals=2; plural=(n != 1);");
